55d9c7f31d0835b25a9d76c4013ad733bf535157
muja-b <s11819212@stu.najah.edu>
2022-07-17 14:31:52 +0300

    Stack using c#

---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿using System; 
+class GFG {
+    static void Main()
+    {
+        Stack<String>.push("Avv");
+        Stack<String>.push("Bcc");
+        Stack<String>.push("Cxx");
+        Stack<String>.push("Dzz");
+        Stack<String>.push("Fqq");
+        Stack<String>.pop();
+        //Stack<String>.print();
+        //System.Console.WriteLine(Stack<String>.peak());
+        Stack<String>.clear();
+        Stack<String>.print();
+
+    }
+    class CustomException : Exception
+{
+    public CustomException(string message)
+    {
+    }
+}
+private static void TestThrow()
+{
+    throw new CustomException("No elements to pop");
+}
+ public static class Stack<T> {
+    private static T []arr=new T[10];
+    public static int counter=0;
+    
+  public static void push(T item){
+    arr[counter]=item;
+    counter++;
+    if(counter>10){
+    counter=counter%10;    
+    }
+  }
+  public static T pop(){
+    if(counter==0)TestThrow();
+    counter=counter-1;
+    return arr[counter+1];
+  }
+  public static void print(){
+    for(int i=0;i<counter;i++){
+        if(arr[i]==null)continue;
+        System.Console.WriteLine(arr[i]);
+    }
+  }
+  public static T peak(){
+    return arr[counter-1];
+  }
+  public static void clear(){
+    arr=new T[10];
+    counter=0;
+  }
+}
+}
